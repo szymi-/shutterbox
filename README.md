c57f085124490a56d07526747bb2fdef457b3514
Szymon Jasiński <szymon.jasinski@mirumee.com>
2022-08-13 11:33:10 +0200

    removing old version

--- README.md
+++
@@ -1,4 +0,0 @@
-Blebox
-------
-
-Tools for managing shutters with Blebox. This tool is obsolete, [I recommend using this instead](https://github.com/szymi-/hass_shutterbox).
--- sql2.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-shutter = ShutterDevice(location='okno1', ip='172.30.1.231')
-session.add(shutter)
-session.commit()
